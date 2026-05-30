ede6addc19deb9ffb333606edf367a8de240bede
Даниил <db228017@gmail.com>
2026-05-31 02:33:50 +0300

    Add project files.

---
+++ TelegramBotTest/Program.cs
@@ -0,0 +1,39 @@
+﻿
+
+using Telegram.Bot.Types;
+using Telegram.Bot;
+
+
+
+
+var bot = new TelegramBotClient("8636785011:AAGU7kyn1OmkDfk8_wwXXMRrREEwGdWC420");
+
+
+bot.StartReceiving(
+    updateHandler: async (bot, update, cancellationToken) =>
+    {
+        if (update.Message is not Message message)
+
+            return;
+
+        Console.WriteLine(message.Text);
+
+        await bot.SendMessage(
+            chatId: message.Chat.Id,
+            text:"Привет"
+            
+            );
+
+    },
+
+    errorHandler: async (bot, Exception, CancellationToken) =>
+    {
+        Console.WriteLine(Exception.Message);
+    }
+
+
+
+
+    );
+Console.WriteLine("Бот запущен");
+Console.ReadLine();
