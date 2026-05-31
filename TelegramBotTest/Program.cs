2ff3b3a9d34e4e64b1958b63d36ed1a1f65361ce
Даниил <db228017@gmail.com>
2026-05-31 20:24:18 +0300

    Добавил что бот может теперь спрашивать про гифки у пользоваталя какую он хочет и пользователь может спросить и бот ему скинет нужную

--- TelegramBotTest/Program.cs
+++ TelegramBotTest/Program.cs
@@ -1,49 +1,66 @@
-﻿
-
+﻿using Telegram.Bot;
 using Telegram.Bot.Types;
-using Telegram.Bot;
+using System.Collections.Generic;
 
-
-
+Dictionary<long, bool> waitingGif = new Dictionary<long, bool>();
 
 var bot = new TelegramBotClient("8636785011:AAGU7kyn1OmkDfk8_wwXXMRrREEwGdWC420");
-
 
 bot.StartReceiving(
     updateHandler: async (bot, update, cancellationToken) =>
     {
         if (update.Message is not Message message)
-
             return;
 
         Console.WriteLine(message.Text);
 
-        if(message.Text == "/start") {
-
-            await bot.SendMessage(message.Chat.Id, "Привет");
-}           
-        else if(message.Text == "/price")
+        if (waitingGif.ContainsKey(message.Chat.Id))
         {
-            await bot.SendMessage(message.Chat.Id, "не знаю такой цены");
+            waitingGif.Remove(message.Chat.Id);
+
+            if (message.Text?.ToLower() == "хомяк")
+            {
+                await bot.SendAnimation(
+                    message.Chat.Id,
+                    InputFile.FromStream(
+                        File.OpenRead(@"F:\Gifs\hamster.mp4")));
+
+                return;
+            }
+
+            await bot.SendMessage(message.Chat.Id, "Такой гифки нет");
+            return;
+        }
+
+        if (message.Text?.ToLower() == "скинь гифку")
+        {
+            waitingGif[message.Chat.Id] = true;
+
+            await bot.SendMessage(message.Chat.Id, "Какую гифку?");
+
+            return;
         }
 
 
+        switch (message.Text)
+        {
+            case "/start":
+                await bot.SendMessage(message.Chat.Id, "Привет");
+                break;
+
+            case "/price":
+                await bot.SendMessage(message.Chat.Id, "Не знаю сколько стоит");
+                break;
+
+
+        }
     },
 
-    errorHandler: async (bot, Exception, CancellationToken) =>
+    errorHandler: async (bot, exception, cancellationToken) =>
     {
-        Console.WriteLine(Exception.Message);
-    
-    
+        Console.WriteLine(exception.Message);
     }
+);
 
-    
-
-
-
-
-
-
-    );
 Console.WriteLine("Бот запущен");
 Console.ReadLine();
