c2c704dcea88f0178257f3e13d0401b97d8f4c97
Даниил <db228017@gmail.com>
2026-06-02 01:45:02 +0300

    Исправил что бот теперь выдает гифки если ему писать просто ему писать в стиле попросить гифку он скинет а не просто одной фразой добавил на будущее в базу данных рандом гифок

--- TelegramBotTest/Program.cs
+++ TelegramBotTest/Program.cs
@@ -1,8 +1,21 @@
 ﻿using Telegram.Bot;
 using Telegram.Bot.Types;
 using System.Collections.Generic;
+using Npgsql;
+
+
+
+string connectionString = "Host=localhost;Port=4321;Username=postgres;Password=Karabas2002;Database=testdb";
 
 Dictionary<long, bool> waitingGif = new Dictionary<long, bool>();
+
+
+await using var connection = new NpgsqlConnection(connectionString);
+await connection.OpenAsync();
+
+Console.WriteLine("База запущена");
+
+
 
 var bot = new TelegramBotClient("8636785011:AAGU7kyn1OmkDfk8_wwXXMRrREEwGdWC420");
 
@@ -14,25 +27,59 @@ bot.StartReceiving(
 
         Console.WriteLine(message.Text);
 
-        if (waitingGif.ContainsKey(message.Chat.Id))
+             if (waitingGif.ContainsKey(message.Chat.Id))
         {
-            waitingGif.Remove(message.Chat.Id);
+            waitingGif.Remove(message.Chat.Id); 
 
-            if (message.Text?.ToLower() == "хомяк")
+            var cmd = new NpgsqlCommand("SELECT path FROM gifs WHERE LOWER(name) = LOWER(@name)", connection);
+
+            cmd.Parameters.AddWithValue("name", message.Text  ?? "");
+
+            Console.WriteLine($"Ищу гифку: {message.Text}");
+
+
+            var result = await cmd.ExecuteScalarAsync();
+
+            Console.WriteLine(result);
+
+            if(result != null)
             {
+
+                var path = result.ToString();
+
+
+                if (!File.Exists(path))
+                {
+                    await bot.SendMessage(message.Chat.Id, "Файл гифки не найден");
+                    return;
+                }
+
                 await bot.SendAnimation(
                     message.Chat.Id,
-                    InputFile.FromStream(
-                        File.OpenRead(@"F:\Gifs\hamster.mp4")));
+                    InputFile.FromStream(File.OpenRead(path)));
+
+                var updateCmd = new NpgsqlCommand(
+                    "UPDATE gifs SET uses = uses + 1 WHERE LOWER(name) = LOWER(@name)",
+                    connection);
+
+                updateCmd.Parameters.AddWithValue("name", message.Text ?? "");
+
+                await updateCmd.ExecuteNonQueryAsync();
 
                 return;
+
             }
 
-            await bot.SendMessage(message.Chat.Id, "Такой гифки нет");
+            await bot.SendMessage(message.Chat.Id, "Гифки с таким названием нет");
+
             return;
+
         }
 
-        if (message.Text?.ToLower() == "скинь гифку")
+
+
+
+        if (message.Text?.ToLower().Contains("гифк") == true)
         {
             waitingGif[message.Chat.Id] = true;
 
@@ -47,6 +94,14 @@ bot.StartReceiving(
             case "/start":
                 await bot.SendMessage(message.Chat.Id, "Привет");
                 break;
+
+
+            case "/gifs":
+                {
+                    await bot.SendMessage(message.Chat.Id, "Пока не завезли");
+                    break;
+                    
+                }
 
             case "/price":
                 await bot.SendMessage(message.Chat.Id, "Не знаю сколько стоит");
